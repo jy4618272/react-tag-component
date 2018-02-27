974ed760242f4c7e9b6c5dce9e8e1b6fa5ac9c49
ZBcoder <zbcoder@aliyun.com>
2018-02-27 19:19:08 +0800

    更改测试埋点

--- demo/src/data/buried.js
+++ demo/src/data/buried.js
@@ -1,9 +1,9 @@
 const data = {
-    'render': 'render buried data',
-    'mount': 'mount buried data',
-    'click': 'click buried data',
-    'slidetop': 'slide top buried data',
-    'slideleft': 'slide left buried data'
+    'render': 'render 函数中埋点',
+    'mount': 'componentdidmount 生命周期埋点',
+    'click': 'click 事件埋点',
+    'slidetop': '上滑 展现埋点',
+    'slideleft': '左滑 展现埋点'
 }
 
 export default data
