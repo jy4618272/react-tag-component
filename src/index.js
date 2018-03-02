083f771ed283721341bc17655d133cae248d7cb8
ZBcoder <zbcoder@aliyun.com>
2018-03-02 10:11:06 +0800

    .

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ const injectData = require('./dataInject').inject
 const injectFunction = require('./functionInject').inject
 
 module.exports = {
+    //数据注入
     get injectData() {
         return injectData
     },
+    //方法注入
     get injectFunction() {
         return injectFunction
     },
+    //初始化
     init(bd) {
         return (fn) => {
             injectData(bd)
             injectFunction(fn)
         }
     },
+    //TD埋点组件
     get TD() {
         return require('./TD').default
     }
